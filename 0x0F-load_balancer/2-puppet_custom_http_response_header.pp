2a5f8b33e7e72b35f602e1ddae54aa90b1878e63
root <richkibz@gmail.com>
2023-04-03 22:10:35 +0300

    use puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,25 @@
 # using puppet
 
 exec {'update':
-  command => '/usr/bin/apt-get update',
+  provider => shell,
+  command  => 'apt-get -y update',
+  before   => Exec['install Nginx'],
 }
--> package {'nginx':
-  ensure => 'present',
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
--> exec {'run':
-  command => '/usr/sbin/service nginx restart',
+
+exec { 'restart Nginx':
+  provider => shell,
+  command => 'service nginx restart',
 }
