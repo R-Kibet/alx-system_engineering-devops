b97026174ee8ec6d61b275f6f143eb5cf3a48112
root <richkibz@gmail.com>
2023-04-24 17:53:30 +0300

    get request

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" get to do list usng REST API """
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    """ find endpoints , user, todo list """
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = argv[1]
+    user = requests.get('{}users/{}'.format(url, userId)).json()
+    todo = requests.get('{}todos?userId={}'.format(url, userId)).json()
+    completed = []
+
+    """ loop to get done tasks """
+    for i in todo:
+        if i.get("completed"):
+            completed.append(i.get("title"))
+    print("Employee {} is done with task({}/{}):"
+          .format(user.get('name'), len(completed), len(todo)))
+    for i in completed:
+        print('\t', i)
