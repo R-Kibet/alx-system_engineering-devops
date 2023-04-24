a28854946653c02a9de92ea9caa7af47bcc19ccc
root <richkibz@gmail.com>
2023-04-24 18:07:42 +0300

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" get to do list usng REST API """
+
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == '__main__':
+    """ find endpoints , user, todo list """
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = argv[1]
+    user = requests.get('{}users/{}'.format(url, userId)).json()
+    todo = requests.get('{}todos?userId={}'.format(url, userId)).json()
+
+    """ export to csv """
+    with open('{}.csv'.format(userId), 'w') as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for i in todo:
+            write.writerow([int(userId), user.get(
+                'username'), i.get('completed'), i.get('title')])
