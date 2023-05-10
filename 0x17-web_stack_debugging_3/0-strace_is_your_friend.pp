c202d21727e9b912d0dadc7c5cd4bba7d3eba3de
root <richkibz@gmail.com>
2023-05-10 19:22:07 +0300

    debuging derver error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # error file name .phpp change to  .php
-exec { "debug":
-  command => 'sed -i "s/.phpp/.php" /var/www/html/wp-settings.php,
+exec { 'debug':
+  command  => 'sed -i "s/.phpp/.php" /var/www/html/wp-settings.php',
   provider => shell,
   }
