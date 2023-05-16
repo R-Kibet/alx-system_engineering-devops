c0a206cea4e0ac9e4f6c100875d90cfb48c39b65
root <richkibz@gmail.com>
2023-05-16 19:27:23 +0300

    fix failed req

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# solving request failded prolem
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx; sudo service nginx restart',
+}
